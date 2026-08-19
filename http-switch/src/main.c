e992a43a389a0723aa64d91ed7b22868c1415cd0
Ignacio <ignaciovidalhernando@gmail.com>
2026-08-19 13:29:33 +0200

    Add enum size example

--- http-switch/src/main.c
+++ http-switch/src/main.c
@@ -8,5 +8,7 @@ int main(void) {
     printf("%s\n", http_to_str(HTTP_TEAPOT));
     printf("%s\n", http_to_str(HTTP_INTERNAL_SERVER_ERROR));
 
+    printf("The size of http_error_code_t is %zu bytes\n", sizeof(http_error_code_t));
+
     return 0;
 }
